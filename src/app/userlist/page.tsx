f27ef260bfa44ba4da57c35b28b3eabb818d37d4
MadJangE00 <jiniixz@naver.com>
2025-04-24 16:35:23 +0900

    second edit

--- src/app/userlist/page.tsx
+++ src/app/userlist/page.tsx
@@ -3,17 +3,5 @@ import { supabase } from "@/lib/supabaseClient";
 
 
 export default function UserListPage() {
-    const userListdata = async() => {
-        const {data, error } = await supabase
-        .from('users')
-        .select('*')
-        .order('created_at', {ascending:false});
-    }
-
-    return <div>
-        <h1>user list</h1>    
-        
-        </div>
-    
-
+    return 
 }
